785b08ec4691756f089e613ab287c8be4ca438ac
Neztore <gingerjosh007@gmail.com>
2018-02-06 13:51:01 +0000

    Update linkaccount.js

--- Commands/Verification/linkaccount.js
+++ Commands/Verification/linkaccount.js
@@ -1,5 +1,5 @@
 let Polaris = require("../../util/client.js");
-
+const rbx = require("roblox-js");
 class linkAccountCommand extends Polaris.command {
 	constructor(client){
 		super(client);
@@ -9,11 +9,31 @@ class linkAccountCommand extends Polaris.command {
 	}
 	async execute(msg, args) {
 		//If no username, prompt for it.
-		//If there is an existing link 
-		//
-		//
+		var username = args[0];
+		if (!username) {
+			//Prompt for username
+		}
+		var current = this.client.db.users.get(msg.author.id);
+		if (current) {
+			//Either warn them that they already have a link or tell them to do .unlink
+		}
+		
+		//Get ROBLOX userID from username, return error if not existing. Check that error is non-existant user error.
+		try {
+		await rbx.getIdFromUsername();
+		} catch (error) {
+			if (error.message === "ErrorNameForUserNotFOund") {
+				//Say user is invalud
+			} else {
+				//Return that error to the user & sentry?
+			}
+			
+		}
+		//Generate code, add to queue and return it.
+		
 
 	}
+	generate
 
 }
 module.exports = linkAccountCommand;
