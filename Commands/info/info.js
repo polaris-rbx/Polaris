1ff9ef08e4c333cac22c411acda5bb9cc476ce31
Neztore <josh@muir.xyz>
2018-06-10 22:07:36 +0100

    Updated .info to include users and servers

--- Commands/info/info.js
+++ Commands/info/info.js
@@ -1,5 +1,5 @@
 let Polaris = require('../../util/client.js')
-
+const packageJson = require('../../package.json')
 class infoCommand extends Polaris.command {
   constructor (client) {
     super(client)
@@ -12,14 +12,19 @@ class infoCommand extends Polaris.command {
     msg.channel.sendInfo(msg.author,
       {
         title: 'Bot information',
-        description: '**Polaris** is a ROBLOX verification bot created by `Neztore#6998`. The bot is always undergoing improvement, if you have any suggestions for an improvement that could be made please join our discord.\nCurrent version: v3.1.0',
+        description: '**Polaris** is a ROBLOX verification bot created by `Neztore#6998`. The bot is always undergoing improvement, if you have any suggestions for an improvement that could be made please join our discord.\nCurrent version: v' + packageJson.version,
         timestamp: new Date(),
         fields: [
           {name: 'Bot info', value: 'Polaris was created by Neztore. It is written in Node.js. If you wish to view the bot commands, please do `.help`.'},
           {name: 'Discord invite', value: 'For support, join our [discord](https://discord.gg/eVyVK5J). This is also where you can make suggestions.'},
           {name: 'Bot invite', value: 'To invite the bot to your serever, use [this](https://discordapp.com/oauth2/authorize?client_id=375408313724043278&scope=bot&permissions=470281408) link.'},
           {name: 'Donate', value: 'Do you enjoy using Polaris? Please donate to the running of the bot [here](https://www.paypal.me/Neztore).'},
-          {name: 'Discord bot list', value: 'Why not check us out on [Discord bot list](https://discordbots.org/bot/375408313724043278)? You can also vote for Polaris, but we don\'t mind!'}
+          {name: 'Discord bot list', value: 'Why not check us out on [Discord bot list](https://discordbots.org/bot/375408313724043278)? You can also vote for Polaris, but we don\'t mind!'},
+          {name: 'Servers', value: this.client.guilds.size, inline: true},
+          {name: 'Users', value: this.client.users.size, inline: true},
+          {name: 'Uptime', value: `${Math.round(this.client.uptime / 60000)} Minutes`, inline: true}
+
+
         ]
       })
   }
